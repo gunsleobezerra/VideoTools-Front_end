036844fc044d8498222f1325cc5d24dd0ba1a062
Leonardo Bezerra <leosoares.bezerra2017@gmail.com>
2023-11-26 20:43:42 -0300

    atualizando videos

--- flaskvue/src/components/ListaVideos.vue
+++ flaskvue/src/components/ListaVideos.vue
@@ -17,6 +17,29 @@ videosApi.getVideos().then((response) => {
 
  const isLoading = ref(false);
 
+ function deleteVideo (event: any) {
+   let Video: Video = {
+     id: event.target.parentElement.id,
+     name: event.target.parentElement.children[0].innerText,
+     description: event.target.parentElement.children[1].innerText,
+     url: event.target.parentElement.children[2].innerText
+   }
+
+    videosApi.deleteVideo(Video).then((response) => {
+      console.log(response)
+      
+      let card = document.getElementById(Video.id.toString())
+      card?.remove()
+      }
+    
+    )
+
+
+
+   
+    
+
+}
  
 
 </script>
@@ -24,23 +47,28 @@ videosApi.getVideos().then((response) => {
 <template>
 
     
-<div v-if="isLoading" class="row row-cols-1 row-cols-md-2 g-4">
+<div v-if="isLoading && cards.length!=0" class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col" v-for="card in cards">
     <div  class="card mycard">
-      <div class="card-body">
+      <div class="card-body" :id="card.id.toString()">
         
         <h5 class="card-title">{{ card.name }}</h5>
         <p class="card-text">{{ card.description!="" ? card.description : "No description" }}</p>
         <a :href="card.url">{{ card.url }}</a>
+        <button type="button" class="btn btn-danger" @click="deleteVideo">Delete</button>
       </div>
     </div>
   </div>
 </div>
 
-<div v-else class="d-flex justify-content-center">
+<div v-else="cards.length!=0" class="d-flex justify-content-center">
   <div class="spinner-border" role="status">
     <span class="visually-hidden">Loading...</span>
   </div>
+</div>
+
+<div v-else class="d-flex justify-content-center">
+  <h1>No videos</h1>
 </div>
 
 
